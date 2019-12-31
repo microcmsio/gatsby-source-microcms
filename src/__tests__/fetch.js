c3b8afeffaafcfd959f87fd5535610a907810ef4
shooontan <logo.bee.doo19@gmail.com>
2019-12-31 13:26:22 +0900

    use node-fetch

--- src/__tests__/fetch.js
+++ src/__tests__/fetch.js
@@ -7,55 +7,46 @@ const nockBase = nock(baseUrl, {
   reqheaders: {
     'x-api-key': apiKey,
   },
-});
+}).matchHeader('x-api-key', apiKey);
 
 describe('fetch', () => {
-  test('success fetch', async () => {
-    nockBase.get('/success').reply(200, { contents: [] });
-    const res = await fetchData(baseUrl + '/success', { apiKey });
+  test('default', async () => {
+    nockBase.get('/default').reply(200, { contents: [] });
+    const res = await fetchData(baseUrl + '/default', { apiKey });
     expect(res.statusCode).toBe(200);
-    expect(res.req.headers['x-api-key']).toBe(apiKey);
   });
 
-  test('query fetch', async () => {
+  test('query', async () => {
     nockBase
       .get('/query')
       .query({ fields: 'id' })
-      .reply(200, { contents: [{ id: '1' }] })
-      .get('/noquery')
-      .reply(200, { contents: [{ id: '2' }] })
-      .get('/emptyquery')
-      .reply(200, { contents: [{ id: '3' }] });
-
-    // with query
-    const res1 = await fetchData(baseUrl + '/query', {
+      .reply(200, { contents: [{ id: '1' }] });
+    const res = await fetchData(baseUrl + '/query', {
       apiKey,
       query: { fields: 'id' },
     });
-    expect(res1.statusCode).toBe(200);
-    expect(res1.body.contents[0]['id']).toBe('1');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.contents[0]['id']).toBe('1');
+  });
 
-    // without query
-    const res2 = await fetchData(baseUrl + '/noquery', {
-      apiKey,
-    });
-    expect(res2.statusCode).toBe(200);
-    expect(res2.body.contents[0]['id']).toBe('2');
-
-    // empty query
-    const res3 = await fetchData(baseUrl + '/emptyquery', {
+  test('delete query', async () => {
+    nockBase.get('/deletequery').reply(200, { contents: [{ id: '1' }] });
+    const res = await fetchData(baseUrl + '/deletequery', {
       apiKey,
       query: {
-        fields: '',
-        limit: undefined,
+        // should be deleted
+        draftKey: '',
+        fields: undefined,
+        limit: false,
         offset: null,
       },
     });
-    expect(res3.statusCode).toBe(200);
-    expect(res3.body.contents[0]['id']).toBe('3');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.contents[0]['id']).toBe('1');
   });
 
-  // TODO
-  // test('error fetch', async () => {
-  // });
+  test('error fetch', async () => {
+    nockBase.get('/error').reply(400);
+    await expect(fetchData(baseUrl + '/error', apiKey)).rejects.toThrow();
+  });
 });
--- src/fetch.js
+++ src/fetch.js
@@ -1,6 +1,12 @@
 const qs = require('querystring');
-const got = require('got');
+const fetch = require('node-fetch');
 
+/**
+ * @param {string} url
+ * @param {Object} param
+ * @param {string} param.apiKey
+ * @param {object} param.query
+ */
 module.exports = function fetchData(url, { apiKey, query }) {
   // remove empty string or undefined or null query
   for (let q in query) {
@@ -9,15 +15,15 @@ module.exports = function fetchData(url, { apiKey, query }) {
     }
   }
 
-  return got
-    .get(url, {
-      headers: {
-        'x-api-key': apiKey,
-      },
-      json: true,
-      query: qs.stringify(query) || undefined,
-    })
-    .catch(error => {
-      return error.response;
-    });
+  const params = qs.stringify(query);
+  const reqUrl = url + (params ? `?${params}` : '');
+
+  return fetch(reqUrl, {
+    headers: {
+      'x-api-key': apiKey,
+    },
+  }).then(async res => {
+    const body = await res.json();
+    return { body, statusCode: res.status };
+  });
 };

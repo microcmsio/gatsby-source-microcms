88f3e18506c2dd689bb7ddcb2715e8a2eccf9da5
YouheiNozaki <youhey.hey0505@gmail.com>
2021-10-25 15:43:35 +0900

    exampleのコード修正

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 // We prepared one service to make it easy for anyone to develop.
 const MICROCMS_API_KEY = 'dc59f358-4622-471f-8d1e-6c7a6f969558';
-const MICROCMS_GLOBAL_DRAFT_KEY = '0523713e-efeb-4aec-87fd-12c58588c4e3';
 const MICROCMS_SERVICE_ID = 'example';
 
 const { and, equals, exists, notExists } = require('../src/query-builder');
@@ -18,7 +17,6 @@ module.exports = {
       resolve: require.resolve('../gatsby-node'),
       options: {
         apiKey: process.env.MICROCMS_API_KEY || MICROCMS_API_KEY,
-        globalDraftKey: process.env.MICROCMS_GLOBAL_DRAFT_KEY || MICROCMS_GLOBAL_DRAFT_KEY,
         serviceId: process.env.MICROCMS_SERVICE_ID || MICROCMS_SERVICE_ID,
         apis: [
           {
@@ -35,14 +33,14 @@ module.exports = {
                 'updatedAt',
               ].join(','),
               filters: exists('title'),
-            }
+            },
           },
           {
             endpoint: 'gatsbyobject',
-            format: 'object'
-          }
-        ]
+            format: 'object',
+          },
+        ],
       },
-    }
+    },
   ],
 };
